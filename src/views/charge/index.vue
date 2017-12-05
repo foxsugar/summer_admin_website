18a6b30a444324d3966e9ea4f8a2f22d2b67b55e
zbb <1067715297@qq.com>
2017-12-05 18:08:43 +0800

    注释部分表格

--- src/views/charge/index.vue
+++ src/views/charge/index.vue
@@ -43,6 +43,19 @@
 
       </div>
 
+      <div class="block">
+        <span class="demonstration"></span>
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="page_sizes"
+          :page-size="page_size"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="totalPage">
+        </el-pagination>
+      </div>
+
       <el-form-item label="充值金额">
         <el-input v-model="chargeForm.num" placeholder=""></el-input>
       </el-form-item>
@@ -52,6 +65,9 @@
       </el-form-item>
     </el-form>
   </div>
+
+
+
 </template>
 
 
