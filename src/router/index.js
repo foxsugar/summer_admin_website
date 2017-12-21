c40cb0f6fa4f2598718f594cbd4b50b93a45d40e
zhaobin <1067715297@qq.com>
2017-12-21 14:57:08 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -56,17 +56,6 @@ export const asyncRouterMap = [
       { path: 'index', name: '服务器参数', component: _import('constant/index'), meta: { role: ['admin'] }}
     ]
   },
-  // 给代理上下分
-  {
-    path: '/playeragent',
-    component: Layout,
-    redirect: '/playeragent/index',
-    icon: 'yonghuming',
-    noDropdown: true,
-    children: [
-      { path: 'index', name: '代理上下分', component: _import('playeragent/index'), meta: { role: ['admin'] }}
-    ]
-  },
 
   {
     path: '/agent',
